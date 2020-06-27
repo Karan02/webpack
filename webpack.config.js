d776a72069d338214ec9813df99f36cad6efb8ad
Karan02 <krnptl1234@gmail.com>
2020-06-27 11:55:14 +0530

    babel complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 var path = require("path")
 var webpack = require("webpack")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var extractPlugin = new ExtractTextPlugin({
+    filename: "main.css"
+})
 module.exports = {
     entry:"./src/js/app.js",
     output:{
@@ -10,20 +14,38 @@ module.exports = {
     },
     module:{
         rules: [
+            // {
+            //     test: /\.css$/,
+            //     // loader: "css-loader"
+            //     use:[
+            //         //order is important, webpack loads in reverse order
+            //         // first css-loader will be executed
+            //         "style-loader","css-loader"
+            //     ]
+            // },
             {
-                test: /\.css$/,
-                // loader: "css-loader"
-                use:[
-                    //order is important, webpack loads in reverse order
-                    // first css-loader will be executed
-                    "style-loader","css-loader"
-                ]
+                test: /\.js$/,
+                use: [{
+                    loader:"babel-loader",
+                    options:{
+                        presets:["es2015"]
+                    }
+                }]
+            },{
+                test:/\.scss$/,
+                use: extractPlugin.extract({
+                    // last loader is first compiled
+                    use:[
+                        "css-loader","sass-loader"
+                    ]
+                })
             }
         ]
     },
     plugins:[
         // new webpack.optimize.UglifyJsPlugin({
             
-        // })
+        // }),
+        extractPlugin
     ]
 }
